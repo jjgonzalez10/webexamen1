b21796eb842638873320e779cbb0e90d7af7ba70
jhon <jj.gonzalez10@uniandes.edu.co>
2018-09-21 22:37:07 -0500

    ww

--- front/src/App.js
+++ front/src/App.js
@@ -3,48 +3,98 @@ import './App.css';
 import vegaEmbed from "vega-embed";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      prueba:[],
-      components:[]
+
+    this.state = {
+      myData = [
+        {"a": "A","b": 28}, {"a": "B","b": 55}, {"a": "C","b": 43},
+        {"a": "D","b": 91}, {"a": "E","b": 81}, {"a": "F","b": 53},
+        {"a": "G","b": 19}, {"a": "H","b": 87}, {"a": "I","b": 52}
+      ],
+
+      spec = {
+        $schema: "https://vega.github.io/schema/vega-lite/v2.json",
+        description: "A simple bar chart with embedded data.",
+        data: {
+          name: myData 
+        },
+        mark: "bar",
+        encoding: {
+          y: { "field": "a", "type": "ordinal" },
+          x: { "field": "b", "type": "quantitative" }
+        }
+      }
+
     }
     this.handleChange = this.handleChange.bind(this);
+  };
+
+  componentDidMount(){
+
+    const embed_opt = {"mode": "vega-lite"};    
+    const el = document.getElementById('vis');
+    const view = vegaEmbed("#vis", this.state.spec, embed_opt)
+      .catch(error => console.log(el, error))
+      .then((res) =>  res.view.insert("myData", this.state.myData).run());
   }
-  
+
+  componentDidUpdate(){
+
+    const embed_opt = {"mode": "vega-lite"};    
+    const el = document.getElementById('vis');
+    const view = vegaEmbed("#vis", this.state.spec, embed_opt)
+      .catch(error => console.log(el, error))
+      .then((res) =>  res.view.insert("myData", this.state.myData).run());
+
+  }
+
+
+  handleChange() {
+    try {
+      this.setState({
+        spec:JSON.parse(this.divTarget.value)
+      })
+    } catch (error) {
+      datosJson = { error: 'Error en el spec' };
+
+    }
+  };
+
+
 
   render() {
-    var data = {"url": "https://vega.github.io/schema/vega-lite/v2.json"},
-    const self = this;
+
     return (
-      <div className="App"> 
-  
-      <div><h1>Parcial 1</h1>
-      <textarea>
-        cols="40";
-        rows="20";
-        ref={(div) =>this.divTarget=div}>
-        soy un div
-      </textarea>
-      
-      <button onClick={() =>{
-        
-        var obj= {
-          x: "parcial",
-          y:{field :"vv", "type":"quantitative"}
-        };
-        this.divTarget.value=JSON.stringify(obj,mull,2);
+      <div className="App">
 
-      }
-      }>cambialo
+        <div>
+          <h1>Parcial 1</h1>
+          <h2>JSON</h2>
+          <textarea
+            class name ="JSON"
+            cols="40"
+            rows="20"
+            ref={(div) => this.divTarget = div}>
+          </textarea>
+          <h2>SPEC</h2>
+          <textarea
+            class name ="spec"
+            cols="40"
+            rows="20"
+            ref={(div) => this.divTarget = div}>
+          </textarea>
+          
+          <div id="vis"></div>
+          <button>
+          </button>
 
-      </button>
-
+        </div>
       </div>
-      </div>
-      );
+    );
   }
 
 }
+
 
 export default App;
